0bfa1bf9e2506ed39718f78deac33a7c859f044f
Sid-Agnihotri <56989925+Sid-Agnihotri@users.noreply.github.com>
2021-05-30 13:36:35 -0700

    file structure fixed

--- spotify-clone/src/StateProvider.js
+++
@@ -1,5 +0,0 @@
-import React, { useContext, createContext, useReducer } from "react";
-
-export const StateContext = createContext();
-
-export const StateProvider = ({initialState, reducer}) => ();
--- spotify-clone/src/components/Player.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Player = () => {
-  return <div>Player this isde</div>;
-};
-
-export default Player;
